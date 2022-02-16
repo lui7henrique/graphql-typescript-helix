43ce9e65551d001aed246cbe756f0fe0221aa431
Luiz Henrique <7henrique18@gmail.com>
2022-02-15 21:51:13 -0300

    :sparkles: A simple Query

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,24 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./schema.graphql";
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
+const links: Link[] = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 const resolvers = {
   Query: {
-    info: () => "Test",
+    info: () => "oiiiiiii",
+    feed: () => links,
   },
 };
 
